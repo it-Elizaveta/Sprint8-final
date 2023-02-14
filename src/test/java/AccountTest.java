15e1a5d6560a3bf091a3b98d66ce7ed4b1599810
Elizaveta Agzamova <it-elizaveta@mail.ru>
2023-02-14 23:13:34 +0500

    first commit. program+tests

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(Parameterized.class)
+public class AccountTest {
+
+    private final String name;
+    private final Boolean isExpected;
+
+    public AccountTest (String name, Boolean isExpected) {
+        this.name=name;
+        this.isExpected=isExpected;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getParameters() {
+        return new Object[][] {
+                {"",false},
+                {"p",false},
+                {"po",false},
+                {" ",false},
+                {"TjkjkkTnmmnm",false},
+                {"Thjhhhj HJhjjh Hjjkj",false},
+                {"UKJHG KJKJKKJKJHGHG)",false},
+                {"UKJHG KJKJKoKJKJHGHG)",false},
+                {"q 0",true},
+                {"qi 0",true},
+                {"q77777 0www",true},
+                {"UKJHG KJKJKKJKJHGHG",true},
+        };
+
+    }
+
+    @Test
+    public void checkNameToEmbossParameterizedTest() {
+        Account account= new Account(name);
+        boolean actual= account.checkNameToEmboss();
+        assertEquals(isExpected,actual);
+
+    }
+}
